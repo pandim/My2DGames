09df0e6b9b1d08c73117affbd4a3b071ec2f5c1b
Dmitry Panfilov <d.panfilov@inbox.ru>
2025-05-18 23:43:10 +0300

    13 lesson - Game State, Clear Code

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,7 +15,7 @@ public class Player extends Entity {
     KeyHandler keyH;
     public final int screenX;
     public final int screenY;
-    public int hasKey = 0;
+//    public int hasKey = 0;
     int standCounter = 0;
     boolean moving = false;
     int pixelCounter = 0;
@@ -140,36 +140,7 @@ public class Player extends Entity {
     }
     public void  pickUpObject(int i){
         if (i != 999) {
-            String objectName = gp.obj[i].name;
-            switch (objectName) {
-                case "Key":
-                    gp.playSE(1);
-                    hasKey++;
-                    gp.obj[i] = null;
-                    gp.ui.showMessage("You got a key!");
-                    break;
-                case "Door":
-                    gp.playSE(3);
-                    if (hasKey > 0) {
-                        hasKey--;
-                        gp.obj[i] = null;
-                        gp.ui.showMessage("You opened the door!");
-                    }else {
-                        gp.ui.showMessage("You need a key!");
-                    }
-                    break;
-                case "Boots":
-                    gp.playSE(2);
-                    gp.obj[i] = null;
-                    speed += 2;
-                    gp.ui.showMessage("Speed up!");
-                    break;
-                case "Chest":
-                    gp.ui.gameFinished = true;
-                    gp.stopMusic();
-                    gp.playSE(4);
-                    break;
-            }
+
         }
     }
     public void draw(Graphics g2) {
--- src/main/UI.java
+++ src/main/UI.java
@@ -1,17 +1,15 @@
 package main;
 
-import object.OBJ_Key;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.text.DecimalFormat;
 
 public class UI {
     GamePanel gp;
+    Graphics2D g2;
     Font arial_40, arial_80B;
-    BufferedImage keyImage;
+//    BufferedImage keyImage;
     public boolean messageON = false;
     public String message = "";
     int messageCounter = 0;
@@ -25,8 +23,8 @@ public class UI {
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         // arial_20 = new Font("Arial", Font.PLAIN, 20);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-        OBJ_Key key = new OBJ_Key(gp);
-        keyImage = key.image;
+//        OBJ_Key key = new OBJ_Key(gp);
+//        keyImage = key.image;
 
     }
 
@@ -36,61 +34,29 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
+        this.g2 = g2;
+        g2.setFont(arial_40);
+        g2.setColor(Color.white);
 
-        if (gameFinished) {
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-            String text;
-            int textLength;
-            int x;
-            int y;
-
-            text = "You found the treasure!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - gp.tileSize * 2;
-            g2.drawString(text,x,y);
-
-            text = "You Time is: " + dFormat.format(playTime) + "!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + gp.tileSize * 5;
-            g2.drawString(text,x,y);
-
-            g2.setFont(arial_80B);
-            g2.setColor(Color.yellow);
-            text = "Congratulations!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - gp.tileSize * 3;
-            g2.drawString(text,x,y);
-
-            gp.gameThread = null;
-
-
-        }else{
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, 48, 48, null);
-            g2.drawString("x " + gp.player.hasKey, 74, 65);
-
-            // TIME
-            playTime += (double) 1/60;
-            g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize * 11, 65);
-
-            // MESSAGE
-            if (messageON) {
-                g2.setFont(g2.getFont().deriveFont(30f));
-                g2.drawString(message,gp.tileSize/2, gp.tileSize * 5);
-
-                messageCounter++;
-                if (messageCounter > 120) {
-                    messageCounter = 0;
-                    messageON = false;
-                }
-            }
-
+        if(gp.gameState == gp.playState){
+            // Do playState stuff later
         }
+        if(gp.gameState == gp.pauseState){
+            drawPauseScreen();
+        }
+    }
+    public void drawPauseScreen() {
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));
+        String text = "PAUSED";
+        int x = getXforCenteredText(text);
+        int y = gp.screenHeight / 2;
 
+        g2.drawString(text, x, y);
+
+    }
+    public int getXforCenteredText(String text) {
+        int length = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
+        int x = gp.screenWidth / 2 - length / 2;
+        return x;
     }
 }
